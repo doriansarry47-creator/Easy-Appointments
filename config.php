020ec74b38d7d0ee12ee8ecbfe931624230c6b70
doriansarry47-creator <doriansarry47@gmail.com>
2025-11-15 13:11:14 +0100

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+$db['default'] = array(
+    'hostname' => 'sql206.infinityfree.com',
+    'username' => if0_40425097
+    'password' => 25052000010195
+    'database' => if0_40425097_lollo
+    'dbdriver' => 'mysqli',
+    'pconnect' => FALSE,
+    'db_debug' => TRUE,
+);
